3bdd1aaa338e7001476475d421bb63cc4099ea7c
Zakharov-A-M <morgan.zaharov@yandex.ru>
2017-11-10 15:26:27 +0300

    add js script on page

--- header.php
+++ header.php
@@ -18,24 +18,24 @@ include_once 'api/bd/connectBD.php';
     <link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700&subset=latin,cyrillic'; rel='stylesheet' type='text/css'>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="description" content="База отдыха – это территория где можно забыть о внешнем мире и полностью посвятить время своей семье и друзьям. Устройте незабываемые приключения на Базе отдыха!" />
-    <link href="http://tour-arsenal.ru/bitrix/cache/css/s1/aspro-resort/kernel_main/kernel_main.css?150839992529761" type="text/css"  rel="stylesheet" />
-    <link href="http://tour-arsenal.ru/bitrix/cache/css/s1/aspro-resort/default_95d5eff7abedc35c6060dd1d3ce9311b/default_95d5eff7abedc35c6060dd1d3ce9311b.css?1502657030896" type="text/css"  rel="stylesheet" />
-    <link href="http://tour-arsenal.ru/bitrix/cache/css/s1/aspro-resort/template_ac7466d0103bc99d1e005bfea49e2c5a/template_ac7466d0103bc99d1e005bfea49e2c5a.css?1502659483321481" type="text/css"  data-template-style="true"  rel="stylesheet" />
+    <link href="http://tour-arsenal.by/bitrix/cache/css/s1/aspro-resort/kernel_main/kernel_main.css?150839992529761" type="text/css"  rel="stylesheet" />
+    <link href="http://tour-arsenal.by/bitrix/cache/css/s1/aspro-resort/default_95d5eff7abedc35c6060dd1d3ce9311b/default_95d5eff7abedc35c6060dd1d3ce9311b.css?1502657030896" type="text/css"  rel="stylesheet" />
+    <link href="http://tour-arsenal.by/bitrix/cache/css/s1/aspro-resort/template_ac7466d0103bc99d1e005bfea49e2c5a/template_ac7466d0103bc99d1e005bfea49e2c5a.css?1502659483321481" type="text/css"  data-template-style="true"  rel="stylesheet" />
     <script type="text/javascript">if(!window.BX)window.BX={};if(!window.BX.message)window.BX.message=function(mess){if(typeof mess=='object') for(var i in mess) BX.message[i]=mess[i]; return true;};</script>
     <script type="text/javascript">(window.BX||top.BX).message({'JS_CORE_LOADING':'Загрузка...','JS_CORE_NO_DATA':'- Нет данных -','JS_CORE_WINDOW_CLOSE':'Закрыть','JS_CORE_WINDOW_EXPAND':'Развернуть','JS_CORE_WINDOW_NARROW':'Свернуть в окно','JS_CORE_WINDOW_SAVE':'Сохранить','JS_CORE_WINDOW_CANCEL':'Отменить','JS_CORE_WINDOW_CONTINUE':'Продолжить','JS_CORE_H':'ч','JS_CORE_M':'м','JS_CORE_S':'с','JSADM_AI_HIDE_EXTRA':'Скрыть лишние','JSADM_AI_ALL_NOTIF':'Показать все','JSADM_AUTH_REQ':'Требуется авторизация!','JS_CORE_WINDOW_AUTH':'Войти','JS_CORE_IMAGE_FULL':'Полный размер'});</script>
     <script type="text/javascript">(window.BX||top.BX).message({'LANGUAGE_ID':'ru','FORMAT_DATE':'DD.MM.YYYY','FORMAT_DATETIME':'DD.MM.YYYY HH:MI:SS','COOKIE_PREFIX':'BITRIX_SM','SERVER_TZ_OFFSET':'10800','SITE_ID':'s1','SITE_DIR':'/','USER_ID':'','SERVER_TIME':'1509983944','USER_TZ_OFFSET':'0','USER_TZ_AUTO':'Y','bitrix_sessid':'bcedb80867a78b7f7f108c0963dc83ec'});</script>
 
 
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core.js?1508399900121473"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core_db.js?149890843317880"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core_ajax.js?149890843336424"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/json/json2.min.js?14758345923467"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core_ls.js?147583459210430"></script><!--//-->
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core_fx.js?149890842616888"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core_frame_cache.js?147583459217555"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/jquery/jquery-1.8.3.min.js?147583459293636"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/core/core_popup.js?150839990059295"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/js/main/ajax.js?147583459235509"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core_db.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core_ajax.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/json/json2.min.js?14758345923467"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core_ls.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core_fx.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core_frame_cache.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/jquery/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/core/core_popup.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/js/main/ajax.js"></script>
     <script type="text/javascript">BX.setCSSList(['/bitrix/js/main/core/css/core.css','/bitrix/js/main/core/css/core_popup.css','/bitrix/js/main/core/css/core_date.css','/bitrix/templates/aspro-resort/css/bootstrap.css','/bitrix/templates/aspro-resort/css/fonts/font-awesome/css/font-awesome.min.css','/bitrix/templates/aspro-resort/vendor/flexslider/flexslider.css','/bitrix/templates/aspro-resort/css/jquery.fancybox.css','/bitrix/templates/aspro-resort/css/theme-elements.css','/bitrix/templates/aspro-resort/css/theme-responsive.css','/bitrix/templates/.default/ajax/ajax.css','/bitrix/templates/aspro-resort/styles.css','/bitrix/templates/aspro-resort/template_styles.css','/bitrix/templates/aspro-resort/css/responsive.css','/bitrix/templates/aspro-resort/themes/CUSTOM_s1/colors.css','/bitrix/templates/aspro-resort/components/bitrix/map.yandex.system/.default/style.css']); </script>
     <script>BX.message({'JS_REQUIRED':'Заполните это поле!','JS_FORMAT':'Неверный формат!','JS_FILE_EXT':'Недопустимое расширение файла!','JS_PASSWORD_COPY':'Пароли не совпадают!','JS_PASSWORD_LENGTH':'Минимум 6 символов!','JS_ERROR':'Неверно заполнено поле!','JS_RECAPTCHA_ERROR':'Не подтверждено!','JS_PROCESSING_ERROR':'Согласитесь с условиями!','JS_FILE_SIZE':'Максимальный размер 5мб!','JS_FILE_BUTTON_NAME':'Выберите файл','JS_FILE_DEFAULT':'Файл не найден','JS_DATE':'Некорректная дата!','JS_DATETIME':'Некорректная дата/время!','S_CALLBACK':'Заказать звонок','ERROR_INCLUDE_MODULE_RESORT_TITLE':'Ошибка подключения модуля &laquo;Аспро: Курорт - сайт базы отдыха, санатория, гостиницы&raquo;','ERROR_INCLUDE_MODULE_RESORT_TEXT':'Ошибка подключения модуля &laquo;Аспро: Курорт - сайт базы отдыха, санатория, гостиницы&raquo;.<br />Пожалуйста установите модуль и повторите попытку','S_SERVICES':'Наши услуги','S_SERVICES_SHORT':'Услуги','S_TO_ALL_SERVICES':'Все услуги','S_CATALOG':'Каталог товаров','S_CATALOG_SHORT':'Каталог','S_TO_ALL_CATALOG':'Весь каталог','S_CATALOG_FAVORITES':'Наши товары','S_CATALOG_FAVORITES_SHORT':'Товары','S_NEWS':'Новости','S_TO_ALL_NEWS':'Все новости','S_COMPANY':'О компании','S_OTHER':'Прочее','FANCY_CLOSE':'Закрыть','FANCY_NEXT':'Следующий','FANCY_PREV':'Предыдущий','S_MOBILE_MENU':'Меню','CT_BST_SEARCH_BUTTON':'Найти','HEADER_CONTACTS_LINK':'Схема<br> проезда','CONTACTS_LINK':'Схема проезда','INSTAGRAM_TITLE':'Мы в Instagram','INSTAGRAM_DESCRIPTION':'Отдых в экологически чистом месте, на берегу Лесного озера — это то, о чём мечтает любой горожанин!'})</script>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
@@ -48,33 +48,35 @@ include_once 'api/bd/connectBD.php';
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.actual.min.js?14758352611101"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.fancybox.js?147583526145891"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/blink.js?1475835261347"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/vendor/jquery.easing.js?14758352618097"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/vendor/jquery.appear.js?14758352613188"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/vendor/jquery.cookie.js?14758352612247"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/vendor/bootstrap.js?147583526127908"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/vendor/flexslider/jquery.flexslider-min.js?147583526121817"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/vendor/jquery.validate.min.js?147583526122254"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.uniform.min.js?14758352618308"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jqModal.js?147583526111022"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/detectmobilebrowser.js?14758352612203"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/device.min.js?14758352612605"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.alphanumeric.js?14758352611972"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.waypoints.min.js?14758352618044"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.counterup.js?14758352611069"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.dotdotdot.js?14758352615908"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/general.js?150265927041557"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/custom.js?1476373986523"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/components/bitrix/search.title/script.js?14758349749371"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/js/jquery.inputmask.bundle.min.js?147583526163835"></script>
-    <script type="text/javascript" src="http://tour-arsenal.ru/bitrix/templates/aspro-resort/components/bitrix/map.yandex.view/.default/script.js?14758352611658"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.actual.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.fancybox.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/blink.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/vendor/jquery.easing.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/vendor/jquery.appear.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/vendor/jquery.cookie.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/vendor/bootstrap.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/vendor/flexslider/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/vendor/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.uniform.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jqModal.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/detectmobilebrowser.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/device.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.alphanumeric.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.waypoints.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.counterup.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.dotdotdot.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/general.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/custom.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/components/bitrix/search.title/script.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/js/jquery.inputmask.bundle.min.js"></script>
+    <script type="text/javascript" src="http://tour-arsenal.by/bitrix/templates/aspro-resort/components/bitrix/map.yandex.view/.default/script.js"></script>
     <script type="text/javascript">var _ba = _ba || []; _ba.push(["aid", "bcc93b1d8395e5bd858a567ff5f49a50"]); _ba.push(["host", "tour-arsenal.ru"]); (function() {var ba = document.createElement("script"); ba.type = "text/javascript"; ba.async = true;ba.src = (document.location.protocol == "https:" ? "https://" : "http://") + "bitrix.info/ba.js";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ba, s);})();</script>
 
 
 </head>
 <body>
+<!--<div class="jqmOverlay" style="height: 100%; width: 100%; position: fixed; left: 0px; top: 0px; z-index: 2999; opacity: 0.5;"></div>-->
+
 
 <div id="panel"></div>
 <script type='text/javascript'>
@@ -152,7 +154,7 @@ include_once 'api/bd/connectBD.php';
                     </div>
                     <div class="callback_wrap col-md-5 col-sm-5 col-xs-12">
                         <div class="top-callback">
-                            <div class="callback pull-right hidden-xs" data-event="jqm" data-param-id="22" data-name="callback">
+                            <div class="callback pull-right hidden-xs">
                                 <span class="btn btn-default"><?= $json->header->buttonOrder ?></span>
                             </div>
                             <div class="phone pull-right hidden-xs c_2">
